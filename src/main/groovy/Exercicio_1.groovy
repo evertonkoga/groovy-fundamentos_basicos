bfcbfe3c7f2ac0b8f0c2dff5df5d369226cab78e
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-03 14:14:42 -0300

    commit inicial

---
+++ src/main/groovy/Exercicio_1.groovy
@@ -0,0 +1,10 @@
+class Exercicio_1 {
+    static main(args) {
+        println "Hello world!"
+
+        String name = "Everton Koga"
+        int age = 35
+        String message = "O $name tem $age anos."
+        println message
+    }
+}
---
+++ src/test/groovy/Exercicios.groovy
@@ -0,0 +1,24 @@
+import org.junit.Test
+
+class Exercicios {
+
+    @Test
+    void exercicio1ponto2() {
+        int number = 10
+        println number.toString()
+
+        // Valor literal Integer
+        println number.class
+
+        // Valor literal Long precisa adicionar 'L' ou 'l'
+        println 12l.class.name
+        // Valor literal BigInteger precisa adicionar 'G' ou 'g'
+        println 12g.class.name
+        // Valor literal Float precisa adicionar 'F' ou 'f'
+        println 200.50f.class.name
+        // Valor literal Double precisa adicionar 'D' ou 'd'
+        println 200.50d.class.name
+        // Valor literal BigDecimal
+        println 200.50.class.name
+    }
+}
