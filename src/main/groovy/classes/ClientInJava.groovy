adf596b0a7074b6e38b026010930abccbc144c6a
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-03 16:48:46 -0300

    Constructor overloading in java

--- src/main/groovy/classes/ClientInJava.groovy
+++ src/main/groovy/classes/ClientInJava.groovy
@@ -4,6 +4,19 @@ class ClientInJava {
     private String name;
     private Date createAt;
 
+    ClientInJava(String name) {
+        this.name = name
+    }
+
+    ClientInJava(Date createAt) {
+        this.createAt = createAt
+    }
+
+    ClientInJava(String name, Date createAt) {
+        this.name = name
+        this.createAt = createAt
+    }
+
     public Integer sum(Integer number1, Integer number2) {
         return number1 + number2;
     }
