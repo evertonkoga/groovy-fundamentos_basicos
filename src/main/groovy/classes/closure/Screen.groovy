7c101413238fe97159793ff40c9a06060675effe
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-05 11:24:10 -0300

    created test for Closure dynamic implementation

---
+++ src/main/groovy/classes/closure/Screen.groovy
@@ -0,0 +1,24 @@
+package classes.closure
+
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import java.awt.FlowLayout
+
+class Screen extends JFrame {
+    Screen() {
+        setTitle("Title")
+        setSize(200, 200)
+
+        def button = new JButton("Click")
+        getContentPane().setLayout(new FlowLayout())
+        add(button)
+
+        button.addActionListener({ JOptionPane.showMessageDialog(null, "Foi via closure ") })
+    }
+
+    static main(args) {
+        def screen = new Screen()
+        screen.setVisible(true)
+    }
+}
