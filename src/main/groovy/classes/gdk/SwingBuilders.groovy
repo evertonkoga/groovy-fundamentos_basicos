20c77ec3f0567ea1c92997c6b7f2218c98f0fad9
Everton Fabiano Koga <ekoga@sisprimedobrasil.com.br>
2023-10-06 14:50:18 -0300

    create test for GDK Object Graph Swing Builder

---
+++ src/main/groovy/classes/gdk/SwingBuilders.groovy
@@ -0,0 +1,32 @@
+package classes.gdk
+
+import groovy.swing.SwingBuilder
+
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import java.awt.BorderLayout
+
+class SwingBuilders {
+    static main(args) {
+        SwingBuilder builder = new SwingBuilder()
+        JFrame tela = builder.frame(title: "Tela", size: [300, 120], defaultCloseOperation: JFrame.EXIT_ON_CLOSE,
+                locationRelativeTo: null){
+            panel( constraints: BorderLayout.CENTER) {
+                label(text: "Nome:")
+                textField(id: "nome", columns: 20)
+                label(text: "E-mail:")
+                textField(id: "email", text: "", columns: 20)
+            }
+            panel( constraints: BorderLayout.SOUTH) {
+                button(text: "Gravar", actionPerformed: {
+                    JOptionPane.showMessageDialog(null,
+                            "gravou nome:" + nome.text +" - email:" + email.text)
+                    nome.text = ""
+                    email.text  =""
+                })
+                button(text: "Fechar", actionPerformed: { System.exit(0)})
+            }
+        }
+        tela.setVisible(true)
+    }
+}
